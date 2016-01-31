9cdb392803ab67b7fa0b727d5dfc416cd2edd0be
Iina Lumme <iilumme@cs.helsinki.fi>
2016-01-31 23:16:09 +0200

    metodin poisto

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,10 +3,6 @@ class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-  def average_rating
-    return ratings.average(:score)
-  end
-
   def to_s
     "#{name},  #{brewery.name}"
   end
